4dc4d0fe86e4d8af184fe9e280c196b84dada84d
daniel <daniel.tomchin@deepinstinct.com>
2022-04-06 09:50:59 +0300

    refactor file_handler to handle exceptions internally add more tests

--- filehandler.h
+++ filehandler.h
@@ -16,40 +16,103 @@ class file_handler {
 
 public:
     // constructor
-    file_handler() {};
+    file_handler();
 
     // destructor
-    virtual ~file_handler() { if(fh_fstream.is_open()) {fh_fstream.close();}}
+    virtual ~file_handler() { if(fh_fstream.is_open()) fh_fstream.close();}
 
     // file handler interface
-
     void open(const fs::path& path,std::ios_base::openmode mode = std::ios_base::in|std::ios_base::out);
     bool is_open() { return fh_fstream.is_open();};
-    void close() {fh_fstream.close();}
-    void read(char* buff, std::streamsize count) {fh_fstream.read(buff, count);}
-    void write(const char* buff, std::streamsize count) {fh_fstream.write(buff, count);}
+    void close();
+    void read(char* buff, std::streamsize count);
+    void write(const char* buff, std::streamsize count);
     int sync() {return fh_fstream.sync();}
 
     // positioning
-    void seekg(std::ios::pos_type pos) {fh_fstream.seekg(pos);}
-    void seekg(std::ios::off_type off, std::ios_base::seekdir dir) {fh_fstream.seekg(off, dir);};
-    void seekp(std::ios::pos_type pos) {fh_fstream.seekp(pos);}
-    void seekp(std::ios::off_type off, std::ios_base::seekdir dir) {fh_fstream.seekp(off, dir);}
-
-    // state
-    bool good() const {return fh_fstream.good();}
-    bool eof() const {return fh_fstream.eof();}
-    bool fail() const {return fh_fstream.fail();}
-    bool bad() const {return fh_fstream.bad();}
-    bool operator!() const {return !fh_fstream;}
-    std::ios::iostate exceptions() const {return fh_fstream.exceptions();};
+    void seekg(std::ios::pos_type pos);
+    void seekp(std::ios::pos_type pos);
 
     std::ostream& operator<<(const char* s) {fh_fstream << s << std::endl; return fh_fstream;}
 };
 
-void file_handler::open(const fs::path& path, std::ios::openmode mode) {
-    fh_fstream.open(path.string(), mode);
+file_handler::file_handler() {
     fh_fstream.exceptions(std::ios::failbit | std::ios::badbit | std::ios::eofbit);
+}
+
+void file_handler::open(const fs::path& path, std::ios::openmode mode) {
+    try {
+        fh_fstream.open(path.string(), mode);
+    } catch (std::ios::failure& e) {
+        std::cout << "could not open file, exception caught and handled" << std::endl;
+        fh_fstream.clear();
+    } catch (...) {
+        std::cout << "unexpected exception" << std::endl;
+        throw;
+    }
+
+}
+
+void file_handler::close() {
+    try {
+        fh_fstream.close();
+    } catch (std::ios::failure& e) {
+        std::cout << "could not close file, exception caught and handled" << std::endl;
+        fh_fstream.clear();
+    } catch (...) {
+        std::cout << "unexpected exception" << std::endl;
+        throw;
+    }
+}
+
+void file_handler::write(const char *buff, std::streamsize count) {
+    try {
+        fh_fstream.write(buff, count);
+    } catch (std::ios::failure& e) {
+        std::cout << "could not write to file, exception caught and handled" << std::endl;
+        fh_fstream.clear();
+    } catch (...) {
+        std::cout << "unexpected exception" << std::endl;
+        throw;
+    }
+
+}
+
+void file_handler::read(char *buff, std::streamsize count) {
+    try{
+        fh_fstream.read(buff, count);
+    } catch (std::ios::failure& e) {
+        std::cout << "could not write to file, exception caught and handled" << std::endl;
+        fh_fstream.clear();
+    } catch (...) {
+        std::cout << "unexpected exception" << std::endl;
+        throw;
+    }
+}
+
+void file_handler::seekg(std::ios::pos_type pos) {
+    try{
+        fh_fstream.seekg(pos);
+    } catch (std::ios::failure& e) {
+        std::cout << "could not seek-get in file, exception caught and handled" << std::endl;
+        fh_fstream.clear();
+    } catch (...) {
+        std::cout << "unexpected exception" << std::endl;
+        throw;
+    }
+
+}
+
+void file_handler::seekp(std::ios::pos_type pos) {
+    try{
+        fh_fstream.seekp(pos);
+    } catch (std::ios::failure& e) {
+        std::cout << "could not seek-put in file, exception caught and handled" << std::endl;
+        fh_fstream.clear();
+    } catch (...) {
+        std::cout << "unexpected exception" << std::endl;
+        throw;
+    }
 }
 
 #endif //CPP_TRAINING_NEW_FILEHANDLER_H
--- main.cpp
+++ main.cpp
@@ -3,26 +3,33 @@
 int main() {
 
     char to_print[] = "Some test text";
-    char buff[10];
-    fs::path p("/home/daniel/CLionProjects/cpp_training_new/test_file.txt");
+    char buff[10] = {0};
     file_handler fh;
-    fh.open(p, std::ios::out | std::ios::in | std::ios::trunc);
 
-    // state bits :
-    std::cout << "goodbit : " << fh.good()
-                << ", eofbit : " << fh.eof()
-                << ", failbit : " << fh.fail()
-                << ", badbit : " << fh.bad()
-                << ", exception mask : " << fh.exceptions()
-                << std::endl;
-
+    // open failure test (user does not have write permissions)
+    fs::path p_open_failure("no_w_permissions.txt");
+    fh.open(p_open_failure, std::ios::out);
+    std::cout << "is open : " << fh.is_open() << std::endl;
     fh.seekp(0);
     fh.write(to_print, sizeof(to_print) - 1);
     fh.seekg(5);
     fh.read(buff, 4);
-    buff[4] = '\0';
+    std::cout << "buff : " << buff << std::endl;
 
-    std::cout << buff << std::endl;
+    // open success
+    fs::path p_success("test_file.txt");
+    fh.open(p_success, std::ios::out | std::ios::in | std::ios::trunc);
+    std::cout << "is open : " << fh.is_open() << std::endl;
+
+    // write test
+    fh.seekp(0);
+    fh.write(to_print, sizeof(to_print) - 1);
+
+    // read test
+    fh.seekg(5);
+    fh.read(buff, 4);
+    buff[4] = '\0';
+    std::cout << "buff : " << buff << std::endl;
 
     std::cout << "Finished" << std::endl;
 
